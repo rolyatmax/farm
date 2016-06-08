f18b3fa617006f9beb6ba3b8f1c5d4bfc4f1682b
Taylor Baldwin <taylorbaldwin@gmail.com>
2016-06-07 22:37:31 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# farm
+
+```shell
+npm run serve
+```
+
+For development:
+
+```shell
+npm i
+npm run develop
+```
---
+++ js/main.js
@@ -0,0 +1,9 @@
+import Sketch from 'sketch-js';
+
+
+Sketch.create({
+  container: document.getElementById('wrapper'),
+  setup() {},
+  update() {},
+  draw() {},
+});
